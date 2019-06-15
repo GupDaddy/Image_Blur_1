39e264e7f000043bfc3eee00fe1216ace65b744a
Chris Guthrie <guthricb@gmail.com>
2019-06-15 13:51:35 +0000

    Initial commit

---
+++ image.rb
@@ -0,0 +1,21 @@
+class Image
+	
+	def initialize(pixel)
+		@Images = Array.new(pixel)
+	end
+
+	def output_image
+		@Images.each do |cell|
+			puts cell.join
+		end
+
+	end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
